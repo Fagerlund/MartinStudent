6a57a46d614f5b6241ee2eeb6264ec58d2c2fbdd
Fagerlund <henrik.fagerlund1@telia.com>
2016-05-12 15:13:25 +0200

    Initial commit

---
+++ public/javascripts/JavaScript.js
@@ -0,0 +1,25 @@
+﻿function sendInvite(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log('added');
+        }
+    };
+    
+    try {
+        var nameFirst = document.getElementById('firstname').value;
+        var nameLast = document.getElementById('surname').value;
+        var numberPeople = document.getElementById('number').value;
+        var arrivalStatus = document.querySelector('input[name="Status"]:checked').value;
+        xhttp.open("POST", "registration", true);
+        xhttp.setRequestHeader('Content-Type', 'application/json');
+        xhttp.send(JSON.stringify({ firstname: nameFirst, surname: nameLast, number: numberPeople, status: arrivalStatus }));
+
+        document.getElementById('downHead').style.display = 'block';
+        document.getElementById('main').style.display = 'none';
+    } catch (exception) {
+        // Not all fields had a value
+        document.getElementById('fail').innerHTML = 'Du måste fylla i alla fält';
+    }
+
+}
---
+++ routes/registration.js
@@ -0,0 +1,21 @@
+﻿var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+/* GET users listing. */
+router.post('/', function (req, res) {
+    console.log(req.body);
+    var firstname = req.body.firstname;
+    var surname = req.body.surname;
+    var number = req.body.number;
+    var status = req.body.status;
+    var str = firstname + ',' + surname + ',' + number + ',' + status+','+(new Date()).getTime()+'\n';
+    res.send('okidoki');
+    fs.appendFile('registration.txt', str, function (err) {
+        if (err)
+            console.log(err)
+        else
+            console.log('added "'+str+'" to registration.txt');
+    });
+});
+
+module.exports = router;
