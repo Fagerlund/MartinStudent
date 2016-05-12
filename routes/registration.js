5b88b24d11b5d5bc1ace931fa098553973977f3a
Fagerlund <henrik.fagerlund1@telia.com>
2016-05-12 16:59:02 +0200

    Added browser field to registration, and switched to tab separeted elements

--- routes/registration.js
+++ routes/registration.js
@@ -1,4 +1,5 @@
-﻿var express = require('express');
+﻿var UAParser = require('ua-parser-js');
+var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 /* GET users listing. */
@@ -8,13 +9,21 @@ router.post('/', function (req, res) {
     var surname = req.body.surname;
     var number = req.body.number;
     var status = req.body.status;
-    var str = firstname + ',' + surname + ',' + number + ',' + status+','+(new Date()).getTime()+'\n';
+
+    var browser = '';
+    try {
+        var ua = req.headers['user-agent'];
+        var parser = new UAParser();
+        browser = JSON.stringify(parser.setUA(ua).getBrowser());
+    } catch (ex) { }
+
+    var str = firstname + '\t' + surname + '\t' + number + '\t' + status + '\t' + (new Date()).getTime() + '\t' + browser + '\n';
     res.send('okidoki');
     fs.appendFile('registration.txt', str, function (err) {
         if (err)
             console.log(err)
         else
-            console.log('added "'+str+'" to registration.txt');
+            console.log('added "' + str + '" to registration.txt');
     });
 });
 
